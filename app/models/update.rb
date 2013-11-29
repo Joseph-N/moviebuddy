efe558957d9fa1f43c4127483407df76e0f529a2
Joseph-N <josendungu@hotmail.com>
2013-11-29 08:49:40 +0300

    restructuring updates table

--- app/models/update.rb
+++ app/models/update.rb
@@ -6,7 +6,12 @@ class Update < ActiveRecord::Base
 	belongs_to :user
 	has_many :update_comments
 
-	validates_presence_of :user_id, :content
+	validates_presence_of :user_id
+	validates_presence_of :content, :if => :not_from_movie?
 
 	default_scope { order('updates.created_at DESC') }
+
+	def not_from_movie?
+		self.movie.equal?(0) 
+	end
 end
